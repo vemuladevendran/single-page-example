a34e3aa9e5e05e6ca20990b0ca6959b5f0271e81
devendran <devendranvemula@gmail.com>
2021-07-21 15:11:34 +0530

    axios added

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,64 +1,74 @@
-import { Component } from 'react';
+import { PureComponent } from 'react';
+import axios from 'axios';
 import './home.css';
 
-class Home extends Component {
+class Home extends PureComponent {
+    details = [];
 
     constructor(props) {
         super(props);
         this.state = {
-            details: [],
+            tableData: [],
         }
     }
 
     async fetchData() {
         try {
-            const res = await window.fetch('https://jsonplaceholder.typicode.com/users');
-            const data = await res.json();
-            console.log(data)
-            this.setState({
-                details: data,
-            })
+            const res = await axios.get('https://jsonplaceholder.typicode.com/users');
+            this.details = res.data;
         } catch (error) {
-            console.error(error);
+            console.log(error);
         }
-      
     }
 
     componentDidMount() {
-       this.fetchData();
+        this.fetchData();
     }
-
 
     render() {
         return (
-            <div className="table-responsive">
-                <table className="table table-primary table-striped table-bordered">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Name</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">User Name</th>
-                            <th scope="col">Mobile Number</th>
-                            <th scope="col">Web Site</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                           this.state.details.map(x => (
-                                <tr key={x.id}>
-                                    <th scope="row">{x.id}</th>
-                                    <td>{x.name}</td>
-                                    <td>{x.email}</td>
-                                    <td>{x.username}</td>
-                                    <td>{x.phone}</td>
-                                    <td>{x.website}</td>
-                                </tr>
-                            ))
+            <div>
+                <div className="table-responsive">
+                    <table className="table table-primary table-striped table-bordered">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">User Name</th>
+                                <th scope="col">Mobile Number</th>
+                                <th scope="col">Web Site</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                this.state.tableData.map(x => (
+                                    <tr key={x.id}>
+                                        <th scope="row">{x.id}</th>
+                                        <td>{x.name}</td>
+                                        <td>{x.email}</td>
+                                        <td>{x.username}</td>
+                                        <td>{x.phone}</td>
+                                        <td>{x.website}</td>
+                                    </tr>
+                                ))
+                            }
+
+                        </tbody>
+                    </table>
+                </div>
+                <div className="d-flex justify-content-center">
+                    <button className="btn btn-primary" onClick={() => {
+                        if (this.state.tableData.length === this.details.length) {
+                            return;
                         }
 
-                    </tbody>
-                </table>
+                        const tableData = [...this.state.tableData, this.details[this.state.tableData.length]];
+                        this.setState({
+                            tableData,
+                        })
+                    }}>Show next</button>
+                </div>
             </div>
         );
     }
--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -4,12 +4,30 @@ class Signup extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            formValues: {},
+            formValues: {
+                email: '',
+                phone: '',
+                password: '',
+            },
         };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {
-        this.setState({ formValues: event.target.value });
+        this.setState({
+            formValues: {
+                email: event.target.email,
+                phone: event.target.phone,
+                password: event.target.password,
+            },
+        })
+    }
+
+    handleSubmit(event) {
+        console.log(this.state.formValues);
+        event.preventDefault();
     }
     render() {
         return (
@@ -17,11 +35,11 @@ class Signup extends Component {
                 <div className="row justify-content-center align-items-center h-100">
                     <div className="col-12 col-md-4">
                         <div>
-                            <input className="input-fields w-100 my-2" type="email" placeholder='Enter Your Email' name="email" value={this.state.formValues} onChange={this.handleChange}></input>
-                            <input className="input-fields w-100 my-2" type="text" placeholder='phone Number' name="phoneNumber"></input>
-                            <input className="input-fields w-100 my-2" type="password" placeholder='Enter your Password' name="password"></input>
+                            <input className="input-fields w-100 my-2" type="email" placeholder='Enter Your Email' name="email" onChange={this.handleChange}></input>
+                            <input className="input-fields w-100 my-2" type="text" placeholder='phone Number' name="phoneNumber" onChange={this.handleChange}></input>
+                            <input className="input-fields w-100 my-2" type="password" placeholder='Enter your Password' name="password" onChange={this.handleChange}></input>
                             <div className="d-flex justify-content-center my-2">
-                                <button type="button" className="btn btn-primary px-5">Sign Up</button>
+                                <button type="button" className="btn btn-primary px-5" onClick={this.handleSubmit}>Sign Up</button>
                             </div>
                         </div>
                     </div>
